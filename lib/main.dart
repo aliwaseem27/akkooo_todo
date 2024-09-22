741bde6cffc9c7d61944cf3b37f6b8c140fd7485
aliwaseem27 <ali.waseem27@gmail.com>
2024-09-22 22:44:16 +0300

    add: dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:isar/isar.dart';
+import 'package:path_provider/path_provider.dart';
 
 import 'app.dart';
 
-void main() {
+late Isar isar;
+
+Future<void> main() async {
+  // WidgetsFlutterBinding.ensureInitialized();
+  //
+  // final dir = await getApplicationDocumentsDirectory();
+
+  // if (Isar.instanceNames.isEmpty) {
+  //   isar = await Isar.open(
+  //     [],
+  //     directory: dir.path,
+  //   );
+  // }
+
   runApp(const MyApp());
 }
